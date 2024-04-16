b49715a985170672e238f9b3a4cb684397e20758
MagdalenaNitz <magdalenanitz@gmx.de>
2024-04-16 12:55:32 +0200

    .

--- L03_FormElements/CocktailBar/CocktailBar.js
+++ L03_FormElements/CocktailBar/CocktailBar.js
@@ -15,13 +15,13 @@ var L03_CocktailBar;
         // console.log(drink.value);
         // let inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll("input");
         // console.log(inputs);
-        let order = document.querySelector("div#order");
-        order.innerHTML = "";
-        let formData = new FormData(document.forms[0]);
+        let order = document.querySelector("div#order"); //sucht nach einem div, der die id "order" trägt (in HTML)
+        order.innerHTML = ""; // bei jeder Änderung wird das Reingeschriebene gelöscht
+        let formData = new FormData(document.forms[0]); // nimmt aus dem Dokument forms das 0. Formular. Neues Dokument wird angelegt und sofort ausgelegt
         for (let entry of formData) {
-            let item = document.querySelector("[value='" + entry[1] + "']");
-            let price = Number(item.getAttribute("price"));
-            order.innerHTML += item.name + "  € " + price;
+            let item = document.querySelector("[value='" + entry[1] + "']"); // alle entries werden durchgegangen; bei den Einträgen wird gesucht 
+            let price = Number(item.getAttribute("price")); // aus den Einträgen wird Preis geholt/ermittelt
+            order.innerHTML += item.name + "  € " + price; //Name: z.B. Mojito, dahinter steht der zugeordnete Preis
         }
     }
     function displayAmount(_event) {
--- L03_FormElements/CocktailBar/CocktailBar.ts
+++ L03_FormElements/CocktailBar/CocktailBar.ts
@@ -18,15 +18,15 @@ namespace L03_CocktailBar {
         // let inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll("input");
         // console.log(inputs);
 
-        let order: HTMLDivElement = <HTMLDivElement>document.querySelector("div#order");
-        order.innerHTML = "";
+        let order: HTMLDivElement = <HTMLDivElement>document.querySelector("div#order"); //sucht nach einem div, der die id "order" trägt (in HTML)
+        order.innerHTML = ""; // bei jeder Änderung wird das Reingeschriebene gelöscht
 
-        let formData: FormData = new FormData(document.forms[0]);
+        let formData: FormData = new FormData(document.forms[0]); // nimmt aus dem Dokument forms das 0. Formular. Neues Dokument wird angelegt und sofort ausgelegt
         for (let entry of formData) {
-            let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" +  entry[1] + "']");
-            let price: number = Number(item.getAttribute("price"));
+            let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" +  entry[1] + "']"); // alle entries werden durchgegangen; bei den Einträgen wird gesucht 
+            let price: number = Number(item.getAttribute("price")); // aus den Einträgen wird Preis geholt/ermittelt
 
-            order.innerHTML += item.name + "  € " + price;
+            order.innerHTML += item.name + "  € " + price; //Name: z.B. Mojito, dahinter steht der zugeordnete Preis
         }
     }
 
