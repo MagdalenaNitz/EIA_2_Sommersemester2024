dfa99a50e57eb4a725faf92ffc8c723e3337a7d4
MagdalenaNitz <magdalenanitz@gmx.de>
2024-05-16 16:54:45 +0200

    neuer Versuch

--- L09_Ententeich/duckPond.js
+++ L09_Ententeich/duckPond.js
@@ -3,19 +3,14 @@ var duckPond;
 (function (duckPond) {
     window.addEventListener("load", handleLoad);
     let golden = 0.62;
-    let clouds = [];
     function handleLoad(_event) {
         let canvas = document.querySelector("canvas");
         if (!canvas)
             return;
         duckPond.crc2 = canvas.getContext("2d");
-        for (let i = 0; i < 4; i++) {
-            let cloud = new duckPond.Cloud(100, 200);
-            cloud.draw;
-            clouds.push(cloud);
-        }
         drawBackground();
         drawSun({ x: 70, y: 70 });
+        drawCloud({ x: 120, y: 45 }, { x: 200, y: 70 });
         drawMountains();
     }
     function drawBackground() {
@@ -40,50 +35,64 @@ var duckPond;
         duckPond.crc2.arc(0, 0, r2, 0, 2 * Math.PI);
         duckPond.crc2.fill();
     }
+    function drawCloud(_position, _size) {
+        console.log("Cloud", _position, _size);
+        let nParticles = 35; //Anzahl der Partikel
+        let radiusParticle = 40; //Radius einzelner Partikel
+        //Pfad erstellen
+        let particle = new Path2D();
+        let gradient = duckPond.crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
+        particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
+        gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.5)");
+        gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
+        duckPond.crc2.save();
+        duckPond.crc2.translate(_position.x, _position.y);
+        duckPond.crc2.fillStyle = gradient;
+        for (let drawn = 0; drawn < nParticles; drawn++) {
+            duckPond.crc2.save();
+            let x = (Math.random() - 0.5) * _size.x; //Koordinatensystem verschieben - Zufallszahl zwischen -0.5 und 0.5
+            let y = (Math.random() * _size.y); //Zufallszahl zwischen 0 und 1
+            duckPond.crc2.translate(x, y); //Koordinatensystem an richtige Stelle schieben
+            duckPond.crc2.fill(particle);
+            duckPond.crc2.restore();
+        }
+    }
     function drawMountains() {
         console.log("Mountains");
         let color = "#aaaaaa";
         duckPond.crc2.save();
         duckPond.crc2.beginPath();
-        duckPond.crc2.translate(0, 250);
+        duckPond.crc2.translate(0, 230);
         duckPond.crc2.fillStyle = color;
         duckPond.crc2.beginPath();
-        duckPond.crc2.moveTo(-115, 0);
+        duckPond.crc2.moveTo(-250, 0);
         duckPond.crc2.lineTo(390, 0);
         duckPond.crc2.lineTo(390, -50);
         duckPond.crc2.lineTo(300, -80);
         duckPond.crc2.lineTo(190, -55);
         duckPond.crc2.lineTo(130, -80);
         duckPond.crc2.lineTo(70, -60);
-        duckPond.crc2.lineTo(0, -90);
+        duckPond.crc2.lineTo(-50, -110);
         duckPond.crc2.closePath();
         duckPond.crc2.fill();
         duckPond.crc2.restore();
         duckPond.crc2.save();
         duckPond.crc2.beginPath();
-        duckPond.crc2.translate(0, 260);
+        duckPond.crc2.translate(0, 240);
         duckPond.crc2.fillStyle = "grey";
         duckPond.crc2.beginPath();
-        duckPond.crc2.moveTo(-110, 0);
+        duckPond.crc2.moveTo(-250, 0);
         duckPond.crc2.lineTo(390, 0);
         duckPond.crc2.lineTo(390, -60);
-        duckPond.crc2.lineTo(330, -30);
-        duckPond.crc2.lineTo(240, -55);
+        duckPond.crc2.lineTo(310, -50);
+        duckPond.crc2.lineTo(220, -40);
         duckPond.crc2.lineTo(170, -35);
         duckPond.crc2.lineTo(100, -65);
         duckPond.crc2.lineTo(50, -40);
-        duckPond.crc2.lineTo(0, -60);
+        duckPond.crc2.lineTo(-50, -60);
         duckPond.crc2.closePath();
         duckPond.crc2.fill();
         duckPond.crc2.restore();
     }
-    function pseudoRandom(seed) {
-        let value = seed;
-        return function () {
-            value = (value * 9301 + 49297) % 233280;
-            return value / 233280;
-        };
-    }
-    duckPond.pseudoRandom = pseudoRandom;
 })(duckPond || (duckPond = {}));
 //# sourceMappingURL=duckPond.js.map
--- L09_Ententeich/duckPond.ts
+++ L09_Ententeich/duckPond.ts
@@ -8,7 +8,6 @@ namespace duckPond {
         export let crc2: CanvasRenderingContext2D;
         let golden: number = 0.62;
 
-        let clouds: Cloud[] = [];
 
     function handleLoad(_event: Event): void {
         let canvas: HTMLCanvasElement | null = document.querySelector("canvas");
@@ -16,14 +15,9 @@ namespace duckPond {
             return;
         crc2 = <CanvasRenderingContext2D>canvas.getContext("2d");
 
-        for (let i: number = 0; i<4; i++) {
-            let cloud: Cloud = new Cloud (100, 200);
-            cloud.draw;
-            clouds.push(cloud);
-        }
-
         drawBackground();
         drawSun({ x: 70, y: 70});
+        drawCloud({x:120, y: 45}, {x: 200, y: 70});
         drawMountains();
     }
 
@@ -56,52 +50,73 @@ namespace duckPond {
         crc2.fill();
     }
 
+    function drawCloud(_position: Vector, _size: Vector): void{
+        console.log("Cloud", _position, _size);
+
+        let nParticles: number = 35;        //Anzahl der Partikel
+        let radiusParticle: number = 40;   //Radius einzelner Partikel
+
+        //Pfad erstellen
+        let particle: Path2D = new Path2D();
+        let gradient: CanvasGradient = crc2.createRadialGradient (0, 0, 0, 0, 0, radiusParticle);
+
+        particle.arc(0,0, radiusParticle, 0, 2*Math.PI);
+        gradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.5)");
+        gradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
+
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+
+        for (let drawn: number = 0; drawn < nParticles; drawn++) {
+            crc2.save();
+            let x: number = (Math.random() - 0.5) * _size.x; //Koordinatensystem verschieben - Zufallszahl zwischen -0.5 und 0.5
+            let y: number = (Math.random() * _size.y); //Zufallszahl zwischen 0 und 1
+            crc2.translate(x, y);   //Koordinatensystem an richtige Stelle schieben
+            crc2.fill(particle); 
+            crc2.restore();
+            
+        }
+    }
+
     function drawMountains(): void {
         console.log("Mountains");
         let color: string = "#aaaaaa";
 
         crc2.save();
         crc2.beginPath();
-        crc2.translate(0,250);
+        crc2.translate(0,230);
         crc2.fillStyle = color;
         crc2.beginPath();
-        crc2.moveTo(-115, 0); 
+        crc2.moveTo(-250, 0); 
         crc2.lineTo(390, 0);
         crc2.lineTo(390, -50); 
         crc2.lineTo(300, -80); 
         crc2.lineTo(190, -55);
         crc2.lineTo(130, -80);
         crc2.lineTo(70, -60);
-        crc2.lineTo(0, -90);
+        crc2.lineTo(-50, -110);
         crc2.closePath();
         crc2.fill();
         crc2.restore();
 
         crc2.save();
         crc2.beginPath();
-        crc2.translate(0,260);
+        crc2.translate(0,240);
         crc2.fillStyle = "grey";
         crc2.beginPath();
-        crc2.moveTo(-110, 0); 
+        crc2.moveTo(-250, 0); 
         crc2.lineTo(390, 0);
         crc2.lineTo(390, -60); 
-        crc2.lineTo(330, -30); 
-        crc2.lineTo(240, -55);
+        crc2.lineTo(310, -50); 
+        crc2.lineTo(220, -40);
         crc2.lineTo(170, -35);
         crc2.lineTo(100, -65);
         crc2.lineTo(50, -40);
-        crc2.lineTo(0, -60);
+        crc2.lineTo(-50, -60);
         crc2.closePath();
         crc2.fill();
         crc2.restore();
-    }
-
-    export function pseudoRandom(seed: number): () => number {
-        let value = seed;
-        return function() {
-            value = (value * 9301 + 49297) % 233280;
-            return value / 233280; 
-        };
     }
     
 }  
